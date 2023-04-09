22a6821c632f18dc89d5d8d600920ef8f6c7aabe
arch-spatula <shkim516@naver.com>
2023-04-09 22:37:59 +0900

    feat(App): 라우팅 처리 #5
    - href를 참조하고 조건부렌더링 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
+import { MouseEvent } from "react";
+
 function App() {
-  const handleSignUp = () => {
-    window.location.href = "signup";
+  const handleSignUp = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    window.location.href = "/signup";
   };
-  const handleLogin = () => {
-    window.location.href = "login";
+  const handleSignIn = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    window.location.href = "/signin";
   };
 
-  return (
-    <div className="flex h-screen items-center justify-center gap-4">
-      <button
-        className="box-border flex w-40 border-collapse justify-center self-center rounded border border-green-500 bg-white py-2 text-green-500"
-        onClick={handleSignUp}
-      >
-        회원가입
-      </button>
-      <button
-        className="w-40 rounded bg-green-500 px-4 py-2 text-white"
-        onClick={handleLogin}
-      >
-        로그인
-      </button>
-    </div>
-  );
+  switch (window.location.href) {
+    case "http://localhost:3000/signin":
+      return <div>signin</div>;
+    case "http://localhost:3000/signup":
+      return <div>signup</div>;
+    case "http://localhost:3000/":
+      return (
+        <div className="flex h-screen items-center justify-center gap-4">
+          <button
+            className="box-border flex w-40 border-collapse justify-center self-center rounded border border-green-500 bg-white py-2 text-green-500"
+            onClick={(e) => handleSignUp(e)}
+          >
+            회원가입
+          </button>
+          <button
+            className="w-40 rounded bg-green-500 px-4 py-2 text-white"
+            onClick={handleSignIn}
+          >
+            로그인
+          </button>
+        </div>
+      );
+    default:
+      return <div>404page</div>;
+  }
 }
 
 export default App;
