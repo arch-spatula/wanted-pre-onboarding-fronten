a4b16501f3531c727a77e8470cfb0189ee7361d9
arch-spatula <shkim516@naver.com>
2023-04-10 23:51:41 +0900

    feat(CustomButton): 조건부 testid추가, 비활성화, 스타일링 #4
    - 비활성화를 조건부로 구현
    - 프로퍼티를 조건부로 추가
    - 스타일링을 조건부로 동작

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -3,28 +3,41 @@ interface CustomButtonProps {
   hierarchy: "primary" | "secondary";
   href?: string;
   onClick?: () => void;
+  testId?: string;
+  disabled?: boolean;
 }
 
 /**
  * @param {CustomButtonProps} Props href가 있으면 onClick을 사용하지 않습니다. onClick이 있으면 href를 사용하지 않습니다.
  * @returns {HTMLAnchorElement | HTMLButtonElement}
  * <CustomButton text={buttonText} hierarchy="primary" onClick={() => {}} />
- * <CustomButton text={buttonText} hierarchy="primary" href="/" />
+ * <CustomButton text={buttonText} hierarchy="secondary" href="/" />
  * @see https://www.builder.io/blog/buttons
  * @todo 1. href와 onClick이 호출할 때 상호배타적이도록 타입을 지정합니다.
  */
 
-function CustomButton({ text, href, hierarchy, onClick }: CustomButtonProps) {
+function CustomButton({
+  text,
+  href,
+  hierarchy,
+  onClick,
+  testId,
+  disabled,
+}: CustomButtonProps) {
   let styling = "";
   switch (hierarchy) {
     case "primary":
-      styling =
-        "w-40 rounded bg-green-500 px-4 py-2 text-white flex justify-center";
+      styling = !disabled
+        ? "w-40 rounded bg-green-500 px-4 py-2 text-white flex justify-center hover:bg-green-400 active:bg-green-600"
+        : "pointer-events-none flex w-40 cursor-default justify-center rounded bg-gray-700 px-4 py-2 text-gray-400";
+
       break;
     case "secondary":
-      styling =
-        "box-border flex w-40 border-collapse justify-center self-center rounded border border-green-500 bg-white py-2 text-green-500";
+      styling = !disabled
+        ? "box-border flex w-40 border-collapse justify-center self-center rounded border border-green-500 bg-white py-2 text-green-500 hover:bg-green-50 active:bg-green-100"
+        : "pointer-events-none box-border flex w-40 border-collapse cursor-default justify-center self-center rounded border border-gray-700  bg-white py-2  text-gray-700";
       break;
+
     default:
       styling = "w-40 rounded bg-green-500 px-4 py-2 text-white";
       break;
@@ -32,14 +45,24 @@ function CustomButton({ text, href, hierarchy, onClick }: CustomButtonProps) {
 
   if (href) {
     return (
-      <a className={styling} href={href}>
+      <a
+        className={styling}
+        href={href}
+        {...(testId && { "data-testid": testId })}
+      >
         {text}
       </a>
     );
   }
 
   return (
-    <button className={styling} type="button" onClick={onClick}>
+    <button
+      className={styling}
+      type="button"
+      onClick={onClick}
+      {...(testId && { "data-testid": testId })}
+      {...(disabled && { disabled })}
+    >
       {text}
     </button>
   );
