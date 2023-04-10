9085e0ac4a89c86f9c7341145df6f20f9e442c8e
arch-spatula <shkim516@naver.com>
2023-04-10 13:28:49 +0900

    feat(Signup, Signin): 스타일 및 마크업 #5
    - input, button, heading, paragraph 추가하고 스타일링

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,5 +1,20 @@
 function Signin() {
-  return <main>Sign In</main>;
+  return (
+    <main className="flex h-screen flex-col items-center justify-center gap-4">
+      <div>
+        <h1 className="text-3xl">로그인</h1>
+        <h2>아이디</h2>
+        <input className="w-64 rounded-lg border border-gray-300 px-3 py-2" />
+        <p>헬퍼 텍스트</p>
+        <h2>비밀번호</h2>
+        <input className="w-64 rounded-lg border border-gray-300 px-3 py-2" />
+        <p>헬퍼 텍스트</p>
+        <button className="w-40 rounded bg-green-500 px-4 py-2 text-white">
+          로그인
+        </button>
+      </div>
+    </main>
+  );
 }
 
 export default Signin;
