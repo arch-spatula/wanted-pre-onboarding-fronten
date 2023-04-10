f5b9af0d783dbfbbe757719d304a2bcbf4e4ff37
arch-spatula <shkim516@naver.com>
2023-04-10 13:30:02 +0900

    refactor(Main): 메인 페이지 버튼 순서 변경 #5
    - 위계상 로그인이 먼저이므로 회원가입, 로그인에서 로그인, 회원가입 순으로 변경

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -16,16 +16,16 @@ function Main() {
   return (
     <main className="flex h-screen items-center justify-center gap-4">
       <button
-        className="box-border flex w-40 border-collapse justify-center self-center rounded border border-green-500 bg-white py-2 text-green-500"
-        onClick={handleSignUp}
-      >
-        회원가입
-      </button>
-      <button
         className="w-40 rounded bg-green-500 px-4 py-2 text-white"
         onClick={handleSignIn}
       >
         로그인
+      </button>
+      <button
+        className="box-border flex w-40 border-collapse justify-center self-center rounded border border-green-500 bg-white py-2 text-green-500"
+        onClick={handleSignUp}
+      >
+        회원가입
       </button>
     </main>
   );
