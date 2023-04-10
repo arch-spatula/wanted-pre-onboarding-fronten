113e2dee19f47ca2ce6b8ffd9aa3ca13f3c319dd
arch-spatula <shkim516@naver.com>
2023-04-10 16:46:03 +0900

    refactor(Main): 홈페이지 View만 남기기 #4
    - handle 로직들을 삭제
    - 기존 하드코딩한 버튼을 Custom Button으로 교체
    - 남은 문서의 todo를 삭제

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,32 +1,10 @@
-import { MouseEvent } from "react";
-
-/**
- * @todo 1. 버튼 컴포넌트 분리
- */
+import { CustomButton } from "../components";
 
 function Main() {
-  const handleSignUp = (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    window.location.href = "/signup";
-  };
-  const handleSignIn = (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    window.location.href = "/signin";
-  };
   return (
     <main className="flex h-screen items-center justify-center gap-4">
-      <button
-        className="w-40 rounded bg-green-500 px-4 py-2 text-white"
-        onClick={handleSignIn}
-      >
-        로그인
-      </button>
-      <button
-        className="box-border flex w-40 border-collapse justify-center self-center rounded border border-green-500 bg-white py-2 text-green-500"
-        onClick={handleSignUp}
-      >
-        회원가입
-      </button>
+      <CustomButton text="로그인" hierarchy="primary" href="/signin" />
+      <CustomButton text="회원가입" hierarchy="secondary" href="/signup" />
     </main>
   );
 }
