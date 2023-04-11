8316ee57e2eafd69a83c0c752ac4fd0703d2d85f
arch-spatula <shkim516@naver.com>
2023-04-11 19:12:51 +0900

    feat(Signin, Signup): 유효하지 않을 때 비활성화 #4
    - 유효할 때만 CTA 활성화 기능 추가

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,30 +1,42 @@
 import { CustomButton, CustomInput } from "../components";
+import { useInput } from "../hooks";
+import { checkEmail, checkPassword } from "../utils";
 
 function Signin() {
+  const { inputValues, handleInputChange } = useInput<{
+    id: string;
+    pw: string;
+  }>({ id: "", pw: "" });
+
   return (
     <main className="flex h-screen flex-col items-center justify-center gap-4">
       <h1 className="text-3xl">로그인</h1>
       <div className="flex flex-col gap-4">
         <CustomInput
-          value=""
+          value={inputValues.id}
           placeholder="user@user.com"
-          onChange={(e) => {}}
-          label="아이디"
-          id="아이디"
-          errorMessage="헬퍼 텍스트"
+          onChange={handleInputChange("id")}
+          inputLabel={{ label: "아이디", id: "아이디" }}
+          errorMessage={checkEmail(inputValues.id)}
           testId="email-input"
+          type="email"
         />
         <CustomInput
-          value=""
+          value={inputValues.pw}
           placeholder="8자리 이상 입력해주십시오."
-          onChange={(e) => {}}
-          errorMessage="헬퍼 텍스트"
+          onChange={handleInputChange("pw")}
+          inputLabel={{ label: "비밀번호", id: "비밀번호" }}
+          errorMessage={checkPassword(inputValues.pw)}
           testId="password-input"
+          type="password"
         />
         <CustomButton
           text="로그인"
           hierarchy="primary"
           testId="signin-button"
+          disabled={
+            !!checkEmail(inputValues.id) || !!checkPassword(inputValues.pw)
+          }
         />
       </div>
     </main>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,32 +1,44 @@
 import { CustomButton, CustomInput } from "../components";
+import { useInput } from "../hooks";
+import { checkEmail, checkPassword } from "../utils";
 
 function Signup() {
+  const { inputValues, handleInputChange } = useInput<{
+    id: string;
+    pw: string;
+  }>({ id: "", pw: "" });
+
+  // 유효성 충족 전까지 버튼 비활성화
+
   return (
     <main className="flex h-screen flex-col items-center justify-center gap-4">
       <h1 className="pb-2 text-3xl">회원가입</h1>
       <div className="flex flex-col gap-4">
         <CustomInput
-          value=""
+          value={inputValues.id}
           placeholder="user@user.com"
-          onChange={(e) => {}}
-          label="아이디"
-          id="아이디"
-          errorMessage="헬퍼 텍스트"
+          onChange={handleInputChange("id")}
+          inputLabel={{ label: "아이디", id: "아이디" }}
+          errorMessage={checkEmail(inputValues.id)}
           testId="email-input"
+          type="email"
         />
         <CustomInput
-          value=""
+          value={inputValues.pw}
           placeholder="8자리 이상 입력해주십시오."
-          onChange={(e) => {}}
-          label="비밀번호"
-          id="비밀번호"
-          errorMessage="헬퍼 텍스트"
+          onChange={handleInputChange("pw")}
+          inputLabel={{ label: "비밀번호", id: "비밀번호" }}
+          errorMessage={checkPassword(inputValues.pw)}
           testId="password-input"
+          type="password"
         />
         <CustomButton
           text="회원가입"
           hierarchy="primary"
           testId="signup-button"
+          disabled={
+            !!checkEmail(inputValues.id) || !!checkPassword(inputValues.pw)
+          }
         />
       </div>
     </main>
