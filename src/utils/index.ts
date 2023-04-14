d8b3a16ff5fb9725d79837201672998f01b270b0
arch-spatula <shkim516@naver.com>
2023-04-14 19:39:10 +0900

    feat(setPath, getPath): 유틸 함수 추가 #7
    - 테스트는 보류합니다.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
 import checkEmail from "./checkEmail";
 import checkPassword from "./checkPassword";
 import isValid from "./isValid";
+import getPath from "./getPath";
+import setPath from "./setPath";
 
-export { checkEmail, checkPassword, isValid };
+export { checkEmail, checkPassword, isValid, setPath, getPath };
