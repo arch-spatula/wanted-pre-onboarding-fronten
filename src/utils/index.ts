c74007939e83713f54d740c2bcf3e8849149d76e
arch-spatula <shkim516@naver.com>
2023-04-14 20:25:20 +0900

    feat(checkTakenEmail): checkTakenEmail 구현 #7

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,15 @@
 import checkEmail from "./checkEmail";
 import checkPassword from "./checkPassword";
+import checkTakenEmail from "./checkTakenEmail";
 import isValid from "./isValid";
 import getPath from "./getPath";
 import setPath from "./setPath";
 
-export { checkEmail, checkPassword, isValid, setPath, getPath };
+export {
+  checkEmail,
+  checkPassword,
+  isValid,
+  setPath,
+  getPath,
+  checkTakenEmail,
+};
