d8cb58016bed6ff6d5efcc98c811595267fc2356
arch-spatula <shkim516@naver.com>
2023-04-14 17:18:19 +0900

    feat(signup): signup 함수 구현 #4

---
+++ src/api/signup.ts
@@ -0,0 +1,20 @@
+import { AUTH_PATH, SIGNUP_PATH } from "../constants/constants";
+import { client } from "./client";
+
+async function signup(email: string, password: string) {
+  try {
+    const res = await client.post(
+      AUTH_PATH + SIGNUP_PATH,
+      { email, password },
+      { headers: { "Content-Type": "application/json" } }
+    );
+
+    if (res.status === 201) {
+      return "";
+    }
+  } catch (error) {
+    return "동일한 이메일이 이미 존재합니다.";
+  }
+}
+
+export default signup;
