8c4112c270dcc82b5c4a22c7c4660a8245979ffa
arch-spatula <shkim516@naver.com>
2023-04-15 19:54:04 +0900

    feat(Todo, Router, Singin): 로그인 기능 구현 #8
    - 로그인이 정상적으로 완료되었을 시 /todo 경로로 이동
    - 실패하면 서버 응답을 활용해 에러메시지 제공

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,24 +1,49 @@
+import { useRef, useState } from "react";
 import { signin } from "../api";
 import { CustomButton, CustomInput } from "../components";
+import { TODO_PATH } from "../constants/constants";
 import { useInput } from "../hooks";
-import { checkEmail, checkPassword, isValid } from "../utils";
+import { checkEmail, checkPassword, isValid, setPath } from "../utils";
 
 function Signin() {
   const { email, password } = JSON.parse(
-    sessionStorage.getItem("user") ?? "{ email: '', password: '' }"
+    sessionStorage.getItem("user") ?? '{ "email": "", "password": "" }'
   );
+
   const { inputValues, handleInputChange } = useInput<{
     email: string;
     password: string;
   }>({ email, password });
 
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const [emailErrorMessage, setEmailErrorMessage] = useState("");
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
+
+  const handleSignin = async () => {
+    const res = await signin(inputValues.email, inputValues.password);
+    setEmailErrorMessage("");
+    setPasswordErrorMessage("");
+    switch (res) {
+      case "가입되지 않은 이메일입니다":
+        emailRef.current?.focus();
+        setEmailErrorMessage(res);
+        break;
+      case "비밀번호가 일치하지 않습니다.":
+        passwordRef.current?.focus();
+        setPasswordErrorMessage(res);
+        break;
+      default:
+        localStorage.setItem("token", "Bearer " + res?.access_token);
+        setPath(TODO_PATH);
+        break;
+    }
+  };
+
   const validEmail = checkEmail(inputValues.email);
   const validPassword = checkPassword(inputValues.password);
   const disabled = !isValid(validEmail, validPassword);
-
-  const handleSignin = async () => {
-    await signin(inputValues.email, inputValues.password);
-  };
 
   return (
     <main className="flex h-screen flex-col items-center justify-center gap-4">
@@ -29,7 +54,8 @@ function Signin() {
           placeholder="user@user.com"
           onChange={handleInputChange("email")}
           inputLabel={{ label: "아이디", id: "아이디" }}
-          errorMessage={validEmail}
+          errorMessage={validEmail || emailErrorMessage}
+          customRef={emailRef}
           testId="email-input"
           type="email"
         />
@@ -38,7 +64,8 @@ function Signin() {
           placeholder="8자리 이상 입력해주십시오."
           onChange={handleInputChange("password")}
           inputLabel={{ label: "비밀번호", id: "비밀번호" }}
-          errorMessage={validPassword}
+          errorMessage={validPassword || passwordErrorMessage}
+          customRef={passwordRef}
           testId="password-input"
           type="password"
         />
