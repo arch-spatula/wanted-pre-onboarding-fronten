5010ae78a41f2e1af25a057253c92929edc572d9
arch-spatula <shkim516@naver.com>
2023-04-16 19:08:39 +0900

    feat: 클라이언트 측 todo 추가 기능 구현 #21
    - 기능 추가하면서 필요한 기능 문서화도 추가합니다.

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,7 +1,12 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { CustomButton, CustomInput } from "../components";
 import { useCheckToken, useInput } from "../hooks";
 
+/**
+ * @todo 1. throttle 걸어두기
+ * @todo 2. idx 대신 서버 id 활용하기
+ * @todo 3. 낙관적 업데이트 패턴으로 클라이언트를 먼저 반영하고 나중에 서버와 동기화합니다.
+ */
 function Todo() {
   useCheckToken("토큰 없을 시 Signin으로");
 
@@ -11,14 +16,20 @@ function Todo() {
     todoInput: "",
   });
 
-  const handleCreateTodo = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleCreateTodo = () => {
+    setTodos((prev) => [
+      ...prev,
+      { todo: inputValues.todoInput, isCompleted: false },
+    ]);
+
     resetSpecificInput("todoInput");
   };
 
   return (
     <main className="flex h-screen flex-col items-center justify-center gap-4">
-      <form className="flex h-10 flex-row gap-4" onSubmit={handleCreateTodo}>
+      <form className="flex h-10 flex-row gap-4">
         <CustomInput
           value={inputValues.todoInput}
           placeholder="오늘 배운 일 블로깅하기"
@@ -28,11 +39,20 @@ function Todo() {
         <CustomButton
           text="추가"
           hierarchy="primary"
-          onClick={() => {}}
+          onClick={() => handleCreateTodo()}
           testId="new-todo-add-button"
-          customType="submit"
         />
       </form>
+      <ul>
+        {todos.map((todo, idx) => {
+          return (
+            <li key={idx}>
+              <p>{todo.todo}</p>
+              <input type="checkbox" checked={todo.isCompleted} />
+            </li>
+          );
+        })}
+      </ul>
     </main>
   );
 }
