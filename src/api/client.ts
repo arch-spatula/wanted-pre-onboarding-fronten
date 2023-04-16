01c803da72bfed69683f96873316296eb9eff2f1
arch-spatula <shkim516@naver.com>
2023-04-16 21:39:51 +0900

    doc: client 인스턴스 분리 계획 추가 #21

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,10 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { baseURL } from "../constants/constants";
 
+/**
+ * @todo 1. authClient, todoClient 분리하고 만들기
+ */
+
 const axiosConfig: AxiosRequestConfig = {
   baseURL,
 };
