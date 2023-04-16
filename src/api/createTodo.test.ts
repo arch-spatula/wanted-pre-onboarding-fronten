cec22172dd5943f98605d83453af7d32ee3e3c9e
arch-spatula <shkim516@naver.com>
2023-04-16 21:16:59 +0900

    test: createTodo 통신함수 mocking #21

---
+++ src/api/createTodo.test.ts
@@ -0,0 +1,29 @@
+import { rest } from "msw";
+import { baseURL, TODO_PATH } from "../constants/constants";
+import { server } from "../mocks/server";
+import createTodo from "./createTodo";
+
+describe("createTodo", () => {
+  test("todo 생성 요청 성공", async () => {
+    const task = "과제하기";
+    const todo: Todo = {
+      id: 1,
+      isCompleted: false,
+      todo: task,
+      userId: 1,
+    };
+    expect(await createTodo(task)).toEqual(todo);
+  });
+  test("비어있는 todo 생성 요청", async () => {
+    server.use(
+      rest.post(baseURL + TODO_PATH + "s", (req, res, ctx) =>
+        res(ctx.status(400))
+      )
+    );
+
+    const emptyTask = "";
+    expect(await createTodo(emptyTask)).toEqual(
+      "비어있는 todo 생성을 요청할 수 없습니다."
+    );
+  });
+});
