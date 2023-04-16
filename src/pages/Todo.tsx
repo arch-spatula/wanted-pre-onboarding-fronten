9b2bfbac5cb355dfe8a3878ee1ae35067a642b0c
arch-spatula <shkim516@naver.com>
2023-04-16 18:26:32 +0900

    feat: todo를 입력하고 제출할 수 있게 markup #21

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,11 +1,38 @@
-import { useCheckToken } from "../hooks";
+import { FormEvent } from "react";
+import { CustomButton, CustomInput } from "../components";
+import { useCheckToken, useInput } from "../hooks";
 
 function Todo() {
   useCheckToken("토큰 없을 시 Signin으로");
 
+  const { inputValues, handleInputChange, resetSpecificInput } = useInput<{
+    todoInput: string;
+  }>({
+    todoInput: "",
+  });
+
+  const handleCreateTodo = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    resetSpecificInput("todoInput");
+  };
+
   return (
     <main className="flex h-screen flex-col items-center justify-center gap-4">
-      <div>hello, todo!</div>
+      <form className="flex h-10 flex-row gap-4" onSubmit={handleCreateTodo}>
+        <CustomInput
+          value={inputValues.todoInput}
+          placeholder="오늘 배운 일 블로깅하기"
+          onChange={handleInputChange("todoInput")}
+          testId="new-todo-input"
+        />
+        <CustomButton
+          text="추가"
+          hierarchy="primary"
+          onClick={() => {}}
+          testId="new-todo-add-button"
+          customType="submit"
+        />
+      </form>
     </main>
   );
 }
