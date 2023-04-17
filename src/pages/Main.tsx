fbe1ae254e16675514887b4f4764ce90bd2850af
arch-spatula <shkim516@naver.com>
2023-04-17 15:42:42 +0900

    docs: 사용성 개선 계획 추가 #21

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,10 @@
 import { CustomButton } from "../components";
 
+/**
+ * @todo 1. 조건부 렌더링 추가하기
+ * - token을 보유하면 todo로 route할 수 있는 버튼 제공
+ * - token이 없으면 현재 제공하는 버튼 제공
+ */
 function Main() {
   return (
     <main className="flex h-screen items-center justify-center gap-4">
