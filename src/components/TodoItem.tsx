b877342d85b4ea5458536fc45e25837b5cdfa2f3
arch-spatula <shkim516@naver.com>
2023-04-17 20:38:13 +0900

    feat: 수정 취소시 초기화 #25
    - useInput에 useCallback 적용
    - 수정취소 기능 추가
    - 수정 취소시 원래 input 값으로 초기화

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,4 @@
-import { useId, useState } from "react";
+import { memo, useId, useState } from "react";
 import { useInput, useTodos } from "../hooks";
 import CustomButton from "./common/CustomButton";
 import CustomInput from "./common/CustomInput";
@@ -6,7 +6,9 @@ import CustomInput from "./common/CustomInput";
 function TodoItem({ id, todo, isCompleted }: Todo) {
   const [checked, setChecked] = useState(isCompleted);
   const [isEdit, setIsEdit] = useState(false);
-  const { inputValues, handleInputChange } = useInput<{ editInput: string }>({
+  const { inputValues, handleInputChange, resetSpecificInput } = useInput<{
+    editInput: string;
+  }>({
     editInput: todo,
   });
 
@@ -15,9 +17,8 @@ function TodoItem({ id, todo, isCompleted }: Todo) {
 
   const handleCheck = () => {
     setChecked((prev) => !prev);
+    handleUpdateTodo(id, { todo, isCompleted: checked });
   };
-
-  // console.log(todo);
 
   const handleStartEditTodo = () => {
     setIsEdit(true);
@@ -26,6 +27,11 @@ function TodoItem({ id, todo, isCompleted }: Todo) {
   const handleDoneEditTodo = () => {
     setIsEdit(false);
     handleUpdateTodo(id, { todo: inputValues.editInput, isCompleted: checked });
+  };
+
+  const handleCancelEditTodo = () => {
+    setIsEdit(false);
+    resetSpecificInput("editInput", todo);
   };
 
   return (
@@ -41,34 +47,47 @@ function TodoItem({ id, todo, isCompleted }: Todo) {
           <CustomInput
             value={inputValues.editInput}
             onChange={handleInputChange("editInput")}
+            testId="modify-input"
           />
         ) : (
           <span>{todo}</span>
         )}
       </label>
       {isEdit ? (
-        <CustomButton
-          text="제출"
-          hierarchy="primary"
-          testId="modify-button"
-          onClick={handleDoneEditTodo}
-        />
+        <>
+          <CustomButton
+            text="제출"
+            hierarchy="primary"
+            testId="modify-button"
+            onClick={handleDoneEditTodo}
+          />
+          <CustomButton
+            text="취소"
+            hierarchy="secondary"
+            testId="cancel-button"
+            onClick={handleCancelEditTodo}
+          />
+        </>
       ) : (
-        <CustomButton
-          text="수정"
-          hierarchy="primary"
-          testId="modify-button"
-          onClick={handleStartEditTodo}
-        />
+        <>
+          <CustomButton
+            text="수정"
+            hierarchy="primary"
+            testId="modify-button"
+            onClick={handleStartEditTodo}
+          />
+          <CustomButton
+            text="삭제"
+            hierarchy="secondary"
+            testId="delete-button"
+            onClick={() => handleDeleteTodo(id)}
+          />
+        </>
       )}
-      <CustomButton
-        text="삭제"
-        hierarchy="secondary"
-        testId="delete-button"
-        onClick={() => handleDeleteTodo(id)}
-      />
     </li>
   );
 }
 
-export default TodoItem;
+// 리렌더링이 발생하지 않으면 아래를 풀어둡니다.
+export default memo(TodoItem);
+// export default TodoItem;
