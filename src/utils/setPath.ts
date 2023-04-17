64cb2d46879c653c1e2b7a71fc2bf746c50bb9eb
arch-spatula <shkim516@naver.com>
2023-04-17 15:03:14 +0900

    refactor: href 케이스 확장 #21

--- src/utils/setPath.ts
+++ src/utils/setPath.ts
@@ -1,10 +1,16 @@
-const setPath = (path: string, methodType: "assign" | "replace" = "assign") => {
+const setPath = (
+  path: string,
+  methodType: "assign" | "replace" | "href" = "assign"
+) => {
   switch (methodType) {
     case "assign":
       window.location.assign(path);
       break;
     case "replace":
       window.location.replace(path);
+      break;
+    case "href":
+      window.location.href = path;
       break;
     default:
       window.location.assign(path);
