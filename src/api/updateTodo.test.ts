5f938d3426f80e5fc176e8cd3387c0b37ab8bf27
arch-spatula <shkim516@naver.com>
2023-04-17 14:45:30 +0900

    test: updateTodo 테스트 케이스 추가 #21

---
+++ src/api/updateTodo.test.ts
@@ -0,0 +1,50 @@
+import { rest } from "msw";
+import { baseURL, TODO_ENDPOINT } from "../constants/constants";
+import { server } from "../mocks/server";
+import updateTodo from "./updateTodo";
+
+describe("updateTodo", () => {
+  test("todo 갱신 요청 성공", async () => {
+    expect(
+      await updateTodo(1, {
+        todo: "Hello World",
+        isCompleted: true,
+      })
+    ).toEqual({
+      id: 1,
+      todo: "Hello World",
+      isCompleted: true,
+      userId: 2,
+    });
+  });
+
+  test("토큰 없이 todo 갱신 요청", async () => {
+    server.use(
+      rest.put(baseURL + TODO_ENDPOINT + "/1", (req, res, ctx) =>
+        res(ctx.status(401))
+      )
+    );
+
+    expect(
+      await updateTodo(1, {
+        todo: "Hello World",
+        isCompleted: true,
+      })
+    ).toBe("토큰이 없습니다.");
+  });
+
+  test("접근할 수 없는 todo id에 갱신요청", async () => {
+    server.use(
+      rest.put(baseURL + TODO_ENDPOINT + "/1", (req, res, ctx) =>
+        res(ctx.status(400))
+      )
+    );
+
+    expect(
+      await updateTodo(1, {
+        todo: "Hello World",
+        isCompleted: true,
+      })
+    ).toBe("비어있거나 잘못된 id로 갱신 요청할 수 없습니다.");
+  });
+});
