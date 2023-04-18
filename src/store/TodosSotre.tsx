520d57f04efbb10f6b08c8f1a0257122178fdf45
arch-spatula <shkim516@naver.com>
2023-04-18 21:33:49 +0900

    refactor: #30 #31
    - 통신과 관련된 행위를 먼저 작성합니다.
    - dispatch와 관련된 코드를 다음에 작성합니다.
    - dispatch에서 toggle을 분리합니다. toggle은

--- src/store/TodosSotre.tsx
+++ src/store/TodosSotre.tsx
@@ -5,45 +5,76 @@ import {
   useContext,
   useEffect,
   useMemo,
-  useState,
+  useReducer,
 } from "react";
 import { createTodo, deleteTodo, getTodos, updateTodo } from "../api";
 
+type TodoAction =
+  | { type: "SET_TODOS"; payload: Todo[] }
+  | { type: "ADD_TODO"; payload: Todo }
+  | {
+      type: "UPDATE_TODO";
+      payload: { id: number; todo: string; isCompleted: boolean };
+    }
+  | { type: "DELETE_TODO"; payload: number };
+
+function reducer(state: Todo[], action: TodoAction) {
+  switch (action.type) {
+    case "SET_TODOS":
+      return [...action.payload];
+    case "ADD_TODO":
+      return [...state, action.payload];
+    case "UPDATE_TODO":
+      return [...state].map((todoItem) =>
+        todoItem.id === action.payload.id
+          ? {
+              ...todoItem,
+              todo: action.payload.todo,
+              isCompleted: action.payload.isCompleted,
+            }
+          : todoItem
+      );
+    case "DELETE_TODO":
+      return [...state].filter((todoItem) => todoItem.id !== action.payload);
+    default:
+      return [...state];
+  }
+}
+
 /**
  * @todo 1. 불필요한 리랜더링 이슈 해결
- * @todo 2. reducer로 리팩토링하기
+ * @todo 4. reducer로 리팩토링하기
  */
 function useTodosSource() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, dispatch] = useReducer(reducer, []);
 
   useEffect(() => {
     const syncTodos = async () => {
       const todos = await getTodos();
-      setTodos(todos);
+      dispatch({ type: "SET_TODOS", payload: todos });
     };
     syncTodos();
   }, []);
 
   const handleCreateTodo = useCallback(async (todo: string) => {
     const { id, userId } = await createTodo(todo);
-    setTodos((prev) => [...prev, { id, todo, isCompleted: false, userId }]);
+    dispatch({
+      type: "ADD_TODO",
+      payload: { id, todo, isCompleted: false, userId },
+    });
   }, []);
 
   const handleUpdateTodo = useCallback(
     async (id: number, { todo, isCompleted }: Omit<Todo, "id" | "userId">) => {
-      setTodos((prev) =>
-        [...prev].map((todoItem) =>
-          todoItem.id === id ? { ...todoItem, todo, isCompleted } : todoItem
-        )
-      );
       await updateTodo(id, { todo, isCompleted });
+      dispatch({ type: "UPDATE_TODO", payload: { id, todo, isCompleted } });
     },
     []
   );
 
   const handleDeleteTodo = useCallback(async (id: number) => {
-    setTodos((prev) => [...prev].filter((todoItem) => todoItem.id !== id));
     await deleteTodo(id);
+    dispatch({ type: "DELETE_TODO", payload: id });
   }, []);
 
   const contextValue = useMemo(
