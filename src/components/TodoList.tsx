e29311b5751c2e488fcc01f63a49599efcb0cc4c
arch-spatula <shkim516@naver.com>
2023-04-18 15:48:03 +0900

    fix: TodoProvider 위치 페이지 단위로 변경 #19

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,6 +3,7 @@ import TodoItem from "./TodoItem";
 
 function TodoList() {
   const { todos } = useTodos();
+  console.log(todos);
   return (
     <ul className="flex flex-col gap-4">
       {todos.map(({ todo, isCompleted, id }, idx) => {
